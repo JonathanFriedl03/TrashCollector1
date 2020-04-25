29f9e43a5777d44d999851ae9b080eec10ccdac1
Jonathan Friedl <jfreetz2@gmail.com>
2020-04-25 18:54:00 -0500

    trying to see what changes were made so i can undo it

---
+++ TrashCollector2/Data/Migrations/20200425224907_notSure.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrashCollector2.Data.Migrations
+{
+    public partial class notSure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "94246762-4674-4ac9-9930-5223361e6bd1");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "bf3be44b-1a03-4d07-94fc-d1a421bd41e7");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PickUpDay",
+                table: "Employees",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PickUpDay",
+                table: "Customers",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OneTimePickUp",
+                table: "Customers",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "075dcf69-9db9-4e95-9a5c-9b8896f223ef", "132b95fc-434f-4e81-ae07-a5a088e7e22d", "Customer", "CUSTOMER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "6973791d-bc43-4987-862e-93a47dc75a4a", "87dc0efd-6e8c-4cb6-9e53-6b804c5b98d5", "Employee", "EMPLOYEE" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "075dcf69-9db9-4e95-9a5c-9b8896f223ef");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "6973791d-bc43-4987-862e-93a47dc75a4a");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PickUpDay",
+                table: "Employees",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PickUpDay",
+                table: "Customers",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OneTimePickUp",
+                table: "Customers",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "94246762-4674-4ac9-9930-5223361e6bd1", "a0873ad2-2fec-46f7-a645-0722b01c0137", "Customer", "CUSTOMER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "bf3be44b-1a03-4d07-94fc-d1a421bd41e7", "82f1d900-7aa2-44a9-9cad-219d1c664aec", "Employee", "EMPLOYEE" });
+        }
+    }
+}
